dc0e6e83ee0f9b417aa1261e8838f6c2f349c1ce
Rudreshcg <rudresha.gangaswamaiah@softsuave.com>
2022-11-01 20:01:22 +0530

    31-oct-2022 added some new codes

--- backend/backendapi/api/urls.py
+++ backend/backendapi/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import UserViewSet
+from .views import UserViewSet, BookViewSet
 
 router = routers.DefaultRouter()
 router.register('users', UserViewSet)
+router.register('books', BookViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
